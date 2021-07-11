6757ef7cf2d7f8690fe20cc992871f87cf38a590
Mustakim Kadri <kadri4152@gmail.com>
2021-07-11 11:44:16 -0700

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,6 @@ const setupUI = (user) => {
   if (user) {
     // account info
     db.collection('users').doc(user.uid).get().then(doc => {
-<<<<<<< HEAD
 
       firebase.storage().ref('users/' + user.uid + '/profileImage').getDownloadURL().then(image =>{
         const html = `
@@ -24,16 +23,6 @@ const setupUI = (user) => {
       `;
       accountDetails.innerHTML = html;
       })
-=======
-      const html = `
-        <img style="width: 100px; border-radius: 5rem;" src="${null}"> </img>
-        <br/>
-        <br/>
-        <div style="font-size: 1.2rem;">Logged in as ${user.email}</div>
-        <div style="font-size: 1.2rem;">${doc.data().bio}</div>
-      `;
-      accountDetails.innerHTML = html;
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
     });
     // toggle user UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
@@ -48,18 +37,13 @@ const setupUI = (user) => {
 };
 
 // setup guides
-<<<<<<< HEAD
 const setupGuides = (data) => {
-=======
-const setupGuides = (data, user) => {
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
   guideList.innerHTML = '<h5 class="center-align">No guides avalible.</h5>';
   if (data.length) {
     let html = '';
     data.forEach(doc => {
       const guide = doc.data();
       if(guide.title && guide.content){
-<<<<<<< HEAD
         const li = `
           <li>
               <div class="collapsible-header grey lighten-4"> 
@@ -72,46 +56,17 @@ const setupGuides = (data, user) => {
           </li>
         `;
         html += li;
-=======
-      const li = `
-        <li>
-            <div class="collapsible-header grey lighten-4"> 
-              ${guide.title} 
-              <div class="row"></div>
-              <div class="col s1 right-align"> ${guide.time} </div>
-            </div>
-
-            <div class="collapsible-body white"> ${guide.content} </div>
-        </li>
-      `;
-      html += li;
       }
       else{
         html = guideList.innerHTML = '<h5 class="center-align">No guides avalible.</h5>';
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
       }
     });
     guideList.innerHTML = html
-    
-<<<<<<< HEAD
   }
   else{
-    html = guideList.innerHTML = '<h5 class="center-align">No guides avalible.</h5>';
+      html = guideList.innerHTML = '<h5 class="center-align">No guides avalible.</h5>';
   }
 }
-=======
-  } else {
-    if(user){
-      guideList.innerHTML = '<h5 class="center-align"> No Guides Created</h5>';
-    }
-    else{
-      guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
-    }
-  }
-  
-
-};
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
@@ -122,8 +77,4 @@ document.addEventListener('DOMContentLoaded', function() {
   var items = document.querySelectorAll('.collapsible');
   M.Collapsible.init(items);
 
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
--- scripts/readme.md
+++ scripts/readme.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Need to make it so that the user who created the guide can delete it and possibly re-edit it
 
 
@@ -14,20 +13,3 @@ so that when you press the email it takes you to there bio.
 
 
 
-=======
-Need to make it so that the user who created the guide can delete it and possibly re-edit it
-
-
-Link the email with 
-<a href="#" class="blue-text text-darken-4 modal-trigger" data-target="modal-bio">Bio</a>
-  <div id="modal-bio" class="modal">
-    <div class="modal-content">
-      <h1> Worked </h1>
-    </div>
-  </div>
-
-so that when you press the email it takes you to there bio.
-
-
-
->>>>>>> 37514efbe9e980bb92c4f23d6f7335b46f16bad0
