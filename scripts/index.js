f5d0322ce466f77e96c5e0b54598c08c1fd9cbba
Mustakim Kadri <kadri4152@gmail.com>
2021-07-21 12:11:13 -0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ let profileImage;
 const setupUI = (user) => {
   loadingUI.style.display = 'none'
   if (user) {
-    let noImgSrc = 'https://boostlikes-bc85.kxcdn.com/blog/wp-content/uploads/2019/08/No-Instagram-Profile-Pic.jpg';
+    let noImgSrc = 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg';
     let html;
     let reload = false;
     // account info
